8ce25b66f2deadfb92244b08d70a265c7dd7f68e
Bukola Jimoh <bukola_jimoh@yahoo.co.uk>
2018-07-24 17:49:36 +0100

    connect onExit to Stage close request

--- src/NoteFX.java
+++ src/NoteFX.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
 /**
@@ -18,12 +19,17 @@ public class NoteFX extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("NoteFX.fxml"));
+        FXMLLoader fxml = new FXMLLoader(getClass().getResource("NoteFX.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("NoteFX.fxml"));
+        Parent root = fxml.load();
+        MenuItem exit = (MenuItem) fxml.getNamespace().get("exitFile");
         
         Scene scene = new Scene(root);
         stage.setTitle("Untitled - NoteFX");
         stage.setScene(scene);
         stage.show();
+        
+        stage.setOnCloseRequest(e -> exit.fire());
     }
 
     /**
