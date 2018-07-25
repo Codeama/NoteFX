c10e0fdbd9e8a5473e5317a21ca4f4cbfc256368
Bukola Jimoh <bukola_jimoh@yahoo.co.uk>
2018-07-25 05:44:29 +0100

    method onPrint

--- src/NoteFXController.java
+++ src/NoteFXController.java
@@ -56,6 +56,8 @@ public class NoteFXController {
             fileMenu.closeWindow();
     }
     
+    @FXML public void onPrint(){fileMenu.printText(textArea);}
+    
     
     @FXML
     public void initialize(){
