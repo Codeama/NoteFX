cb8b471fcc8f229a16e33a91168a7f25b8047763
Bukola <bukola_jimoh@yahoo.co.uk>
2018-08-15 05:41:52 +0100

    rename class

---
+++ src/com/NoteFX/FileOutputChooser.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.NoteFX;
+
+import java.io.File;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Bukola
+ */
+public class FileOutputChooser implements FileSystem{
+    
+    @Override
+    public File openDirectory(){
+        
+        FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open File");
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                    new FileChooser.ExtensionFilter("All Files", "*.*"));
+            File file = fileChooser.showOpenDialog(new Stage());
+            
+            return file;
+    }
+    
+}
