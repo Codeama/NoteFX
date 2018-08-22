eaae91bf8c112a14a4be6b7cb3db8532addee4ed
Bukola <bukola_jimoh@yahoo.co.uk>
2018-08-22 08:31:37 +0100

    add comments

--- src/com/NoteFX/NoteFXController.java
+++ src/com/NoteFX/NoteFXController.java
@@ -1,17 +1,7 @@
 package com.NoteFX;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.paint.Color;
-
-
 
 /**
  *
@@ -22,111 +12,103 @@ public class NoteFXController {
        
     @FXML private TextArea textArea;
     @FXML private RadioMenuItem noteFX;
-    @FXML private RadioMenuItem classic;
+    @FXML private ToggleGroup group;
     private final FileIO file = new FileIO();
     private final PrintTask printer = new PrintTask();
     private final Exit exit = new Exit();
     private final StageTitle stage = new StageTitle();
-    
-    private boolean textEdited;
+    /**text change variables**/
     private int textLength;
     private int changeCount;
-    
-    
-    
-    @FXML
-    public void newStage(){
-        if(file.getPathName() != null && textEdited == true)
-            file.showSaveConfirmation(textArea);
-        else{
-            stage.clearStage(textArea);
-            textLength = textArea.getLength();
-            file.setFileName(stage.getFileName()); /*updates file name with 
-                                                           current stage title*/
-        }
-    }
    
+    
+    
+    
+   /***methods begin here***/
+    
+    //open file
     @FXML
     public void open(){
         file.displayFileContent(textArea); 
-        textLength = textArea.getLength(); //gets length of text before changes
+        textLength = textArea.getLength(); 
     }
     
-    /*check method bug for save prompt on exit*/
+    //save as file
     @FXML
     public void save(){
-        textLength = textArea.getLength(); //gets length of text before saving
+        textLength = textArea.getLength(); 
         file.saveContent(textArea);
     } 
-                
+    //save as new file            
     @FXML
     public void saveAsNew(){
-        textLength = textArea.getLength(); //get length of text before saving 
+        textLength = textArea.getLength();  
         file.saveAsNewFile(textArea);
     }
-    
+    //register change(s) to text
     @FXML
-    public void registerTextChange(){ //register changes to text
+    public void registerTextChange(){ 
         int text = textArea.getLength();
         if(textLength != text)
            changeCount++;
     }
-    
-//    private boolean isTextEdited(){
-//        int newLength = textArea.getLength();
-//        
-//        return (newLength != 0 && file.getPathName() == null) ||
-//                (newLength != textLength && file.getPathName() != null) &&
-//                (changeCount > 0);
-//    }
-   
-    @FXML public void exit(){
+    //check if text is edited
+    private boolean isTextEdited(){
         int newLength = textArea.getLength();
         
-        //check text area has content && file doesn't yet exist (not saved)
-        if (newLength != 0 && file.getPathName() == null)
-            textEdited = true;
-        //check current text length has changed && file exists (has been saved)
-        if (newLength != textLength && file.getPathName() != null)
-                textEdited = true;
-
-        //check text changes before exit
-        if (textEdited == true & changeCount > 0 | changeCount > 0){
-            file.showConfirmation(textArea, file.getFileName());
-            if (newLength != 0 && file.getPathName() == null) //re-assess stage
-            textEdited = true;
-            
-            //set a true condition for check
-        System.out.println("changes: " +changeCount);}
-        else
-            exit.closeWindow();
+        return  (newLength != 0 && file.getPathName() == null) |
+                (newLength != textLength && file.getPathName() != null) &&
+                (changeCount > 0);
     }
+    //exit application
+    @FXML public void exit(){
+        if(isTextEdited()){
+            file.showConfirmation(textArea);
+        }
+        else{
+            exit.closeWindow();
+        }
+    }
+    //print text area
+    @FXML public void print(){
+        printer.printText(textArea);}
     
-    @FXML public void print(){printer.printText(textArea);}
-    
+    //create new text area
+    @FXML
+    public void newStage(){
+        if(isTextEdited())
+            file.showSaveConfirmation(textArea);
+        else{
+            stage.clearStage(textArea);}
+            textLength = textArea.getLength();
+            file.resetFileAndPathName(); 
+    }
+   
+    //undo change(s)
     @FXML public void undoChange(){
         textArea.undo();
     }
     
-    @FXML public void changeBackground(){
-        if(noteFX.isSelected()){
-        textArea.setStyle("-fx-control-inner-background:#000000; "
-               + "-fx-font-family: Consolas; -fx-font-size: 16px; -fx-highlight-fill: #00ff00; "
-               + "-fx-highlight-text-fill: #000000; -fx-text-fill: #00ff00; ");
-        }
-        else
-            textArea.setStyle(null);//set this appropriately
-    }
-    
+    //about NoteFX
     @FXML public void info(){
         AboutNoteFX.aboutMe();
     }
     
-    
-    @FXML
+    //theme change
     public void initialize(){
-
+        group.selectedToggleProperty().addListener(
+            (obv, oldValue, newValue) -> {
+                if(newValue == noteFX){
+                    textArea.setStyle("-fx-control-inner-background:#000000; "
+               + "-fx-font-family: Consolas; -fx-font-size: 16px; -fx-highlight-fill: #00ff00; "
+               + "-fx-highlight-text-fill: #000000; -fx-text-fill: #00ff00; ");
+                }
+                else
+                    textArea.setStyle("-fx-control-inner-background:#FFFFFF; "
+               + "-fx-font-family: Consolas; -fx-font-size: 16px; "
+               + "-fx-text-fill: #000000; ");
+        
+    });
     }
-    
      
 }
